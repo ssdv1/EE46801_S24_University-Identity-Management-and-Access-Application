6c707cdfc91c6bf321ffa7a9a9b230e877bd3036
ssdv1 <160284219+ssdv1@users.noreply.github.com>
2024-04-22 15:36:39 -0400

    Delete UML directory

--- UML/myProject/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("dashboard/", views.dashboard, name="dashboard"),
-    path("admin-dashboard", views.admin_dashboard, name ="adminDashboard")
-]
